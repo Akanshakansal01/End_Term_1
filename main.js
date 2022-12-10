853ebef045de05cbf8368362abd1a28606ad28a4
Akanshakansal01 <akanshakansal025@gmail.com>
2022-12-10 15:05:29 +0530

    initial

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const fs=require("f");
-fs.readFile("C:\Users\LENOVO\Desktop\End_Term_1\BOOKS_IMAGES\AliceInWonderland.txt");
+(Array.from(document.getElementsByTagName("aa"))).foreach((aa)=>{
+    console.log("aaaaa");
+    aa.addEventlistener("click",books);
+});
 
-function file(ele,data)
+function books(ele)
 {
-    console.log(data);
+    ele.preventDefault();
+    console.log(ele.target.id);
+    if(ele.target.id=="alice"){
+        reading("C:\Users\LENOVO\Desktop\End_Term_1\BOOKS_IMAGES\AliceInWonderland.txt","Alice In Wonder Land");
+
+    }
+    if(ele.target.id=="lord")
+    {
+        reading("C:\Users\LENOVO\Desktop\End_Term_1\BOOKS_IMAGES\LOTR.txt","Lord of the Rings")
+    }
+    if(ele.target.id=="hyde")
+    {
+        reading("C:\Users\LENOVO\Desktop\End_Term_1\BOOKS_IMAGES\JekyllAndHyde.txt","Jekyll and hyde")
+    }
+}
+
+function reading(url,title)
+{
+    var c=new XMLHttpRequest();
+    c.open('GET',url);
+    c.onreadystatechange=function(){
+        var ti=c.responseText;
+        document.getElementById("bhead").innerText=title;
+        document.getElementById("btext").innerText=ti;
+    }
+    c.send();
 }
